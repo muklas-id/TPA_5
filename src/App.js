a128179ebdf538a863a9b2bd36b4b06055fca506
muklas-id <108504895+muklas-id@users.noreply.github.com>
2022-11-15 14:46:13 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './App.css';
+import List from './List'
+
+class App extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      todoItem: '',
+      items: []
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    event.setState({
+      items: [...this.state.items, this.state.todoItem],
+      todoItem: ''
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      todoItem: event.target.value
+    })
+  }
+
+  render () {
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.todoItem} onChange={this.handleChange} />
+          <button>Tambah</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    )
+  }
+}
+
+export default App;
